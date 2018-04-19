d89dffeb170401dd299d314d28479ee35d02baef
Bystryakov Anton <BystryakonAnton@mail.ru>
2018-04-19 18:59:37 +0300

    added pages using bootstrap

--- app/controllers/rates_controller.rb
+++ app/controllers/rates_controller.rb
@@ -2,22 +2,9 @@ class RatesController < ApplicationController
   def index
     @rates=Rate.all
   end
-  def create
-    @rate=Rate.new(rate_params)
-    if @rate.save
-      redirect_to rates_path
-    else
-      redirect_to new_rate_path
-    end
-  end
-  def new
-    @rate=Rate.new
-  end
+  
   def edit
     @rate=Rate.find(2)
-  end
-  def show
-    @rate=Rate.find(params[:id])
   end
   def update
     @rate=Rate.find(params[:id])
@@ -26,10 +13,6 @@ class RatesController < ApplicationController
     else
       redirect_to root
     end
-  end
-  def destroy
-    @rate=Rate.find(params[:id]).destroy
-    redirect_to rates_path
   end
   private
   def rate_params
